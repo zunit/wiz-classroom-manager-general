d6a2afadf23a3397e8950ccd7e69eb3a557bccd3
UploadWizRobotics <upload@wizrobotics.com>
2025-02-11 00:41:23 -0500

    Improve class finished screen

--- src/activities/ClassEndActivity.jsx
+++ src/activities/ClassEndActivity.jsx
@@ -1,5 +1,10 @@
 function ClassEndActivity() {
-  return <h1>Class is Over!</h1>;
+  return (
+    <>
+      <h1>Class is over!</h1>
+      <p>Thanks for joining our Scratch class this week!</p>
+    </>
+  );
 }
 
 export default ClassEndActivity;
