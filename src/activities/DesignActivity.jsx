3291e8b8c69768194dde7e85ea4fc83674696814
UploadWizRobotics <upload@wizrobotics.com>
2025-02-10 20:46:06 -0500

    Begin implementing DesignActivity component

--- src/activities/DesignActivity.jsx
+++ src/activities/DesignActivity.jsx
@@ -1,5 +1,51 @@
+import React from "react";
+import ToggleButton from "@/components/ui/ToggleButton.jsx";
+import ToggleButtonGroup from "@/components/ui/ToggleButtonGroup.jsx";
+import CustomPrompt from "@/components/activities/CustomPrompt";
+
+function DefaultDesignPrompt() {
+  return <h1>Default Design Prompt Component</h1>;
+}
+
 function DesignActivity() {
-  return <h1>Design Activity</h1>;
+  const [promptType, setPromptType] = React.useState("default");
+
+  function PromptComponent() {
+    let promptComponent;
+    switch (promptType) {
+      case "default":
+        promptComponent = <DefaultDesignPrompt />;
+        break;
+      case "custom":
+        promptComponent = <CustomPrompt />;
+        break;
+      default:
+        promptComponent = <DefaultDesignPrompt />;
+        console.warn(
+          `Invalid prompt type for Design component (was given '${promptType}')`
+        );
+    }
+    return promptComponent;
+  }
+  return (
+    <>
+      <h1>Design Activity</h1>
+      <p>
+        Design a sprite/backdrop with the given theme within the given time
+        limit.
+      </p>
+      <ToggleButtonGroup selection={promptType}>
+        <ToggleButton value="default" onClick={() => setPromptType("default")}>
+          Default
+        </ToggleButton>
+        <ToggleButton value="custom" onClick={() => setPromptType("custom")}>
+          Custom
+        </ToggleButton>
+      </ToggleButtonGroup>
+      <PromptComponent />
+      <p>Once you are ready, start the timer at the top.</p>
+    </>
+  );
 }
 
 export default DesignActivity;
