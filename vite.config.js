1ddeea25d91fde7ad1fd8a349c771926d2ba24a7
UploadWizRobotics <upload@wizrobotics.com>
2025-02-18 12:43:13 -0500

    Fix project configuration so that building produces a single HTML file

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,22 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { viteSingleFile } from "vite-plugin-singlefile";
 import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), viteSingleFile()],
+  build: {
+    target: "esnext",
+    cssCodeSplit: false, // Merge CSS into JS
+    assetsInlineLimit: 100000000, // Large limit to inline assets
+    rollupOptions: {
+      output: {
+        inlineDynamicImports: true,
+      },
+    },
+    minify: "terser", // Ensures JS and CSS are minified
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
