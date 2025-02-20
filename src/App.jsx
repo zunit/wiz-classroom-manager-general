534a990831ad3e8769b73a8826a7eeb5c756b07a
UploadWizRobotics <upload@wizrobotics.com>
2025-02-20 11:18:45 -0500

    Refactor themes and change implementation to use MUI color schemes

--- src/App.jsx
+++ src/App.jsx
@@ -3,50 +3,22 @@ import { AppContext } from "@/context/AppContext";
 import StartingPage from "@/pages/StartingPage";
 import ActivityPage from "@/pages/ActivityPage";
 import EndingPage from "@/pages/EndingPage";
-import { Fab, ThemeProvider, Tooltip, createTheme } from "@mui/material";
+import { Fab, Tooltip, useColorScheme } from "@mui/material";
 import "./App.css";
 
 function App() {
   const { chunks, currentChunkIndex, isClassStarted } =
     React.useContext(AppContext);
-  const [isDarkMode, setIsDarkMode] = React.useState(false);
-  const theme = createTheme({
-    cssVariables: true,
-    palette: {
-      mode: isDarkMode ? "dark" : "light",
-      background: {
-        default: isDarkMode ? "#242424" : "#ffffff",
-        inverse: isDarkMode ? "#ffffff" : "#242424",
-      },
-      text: {
-        primary: isDarkMode
-          ? "rgba(255, 255, 255, 0.87)"
-          : "rgba(0, 0, 0, 0.87)",
-        inverse: isDarkMode
-          ? "rgba(0, 0, 0, 0.87)"
-          : "rgba(255, 255, 255, 0.87)",
-      },
-      action: {
-        hover: isDarkMode ? "#ffffff20" : "#00000010",
-      },
-      border: {
-        default: isDarkMode ? "#ffffff40" : "#00000040",
-      },
-      primary: {
-        main: isDarkMode ? "#8e70fa" : "#6f4ff9",
-      },
-      secondary: {
-        main: isDarkMode ? "#67aba3" : "#00b6a6",
-      },
-      "activity-card": {
-        "pulse-1": isDarkMode ? "#ffffff18" : "#00000010",
-        "pulse-2": isDarkMode ? "#ffffff30" : "#00000020",
-      },
-    },
-  });
+
+  const { mode, setMode } = useColorScheme();
+
+  function handleChangeTheme() {
+    let newMode = mode === "light" ? "dark" : "light";
+    setMode(newMode);
+  }
 
   return (
-    <ThemeProvider theme={theme}>
+    <>
       {!isClassStarted ? (
         <StartingPage />
       ) : currentChunkIndex < chunks.length ? (
@@ -54,17 +26,15 @@ function App() {
       ) : (
         <EndingPage />
       )}
-      <Tooltip title={`Change to ${isDarkMode ? "light" : "dark"} theme`}>
-        <Fab
-          id="toggle-theme"
-          onClick={() => setIsDarkMode((isDarkMode) => !isDarkMode)}
-        >
+
+      <Tooltip title={`Change to ${mode === "light" ? "dark" : "light"} theme`}>
+        <Fab id="toggle-theme" onClick={handleChangeTheme}>
           <span className="material-symbols-rounded">
-            {isDarkMode ? "light_mode" : "dark_mode"}
+            {mode === "light" ? "dark_mode" : "light_mode"}
           </span>
         </Fab>
       </Tooltip>
-    </ThemeProvider>
+    </>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,17 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { AppProvider } from "@/context/AppContext";
+import { ThemeProvider } from "@mui/material";
+import { AppTheme } from "@/themes/AppTheme";
 import App from "@/App.jsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <AppProvider>
-      <App />
+      <ThemeProvider theme={AppTheme} defaultMode="light">
+        <App />
+      </ThemeProvider>
     </AppProvider>
   </StrictMode>
 );
