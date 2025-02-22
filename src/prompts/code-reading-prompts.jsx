9cc52ced7ae3884407ccebed5b45d329c919e19f
UploadWizRobotics <upload@wizrobotics.com>
2025-02-22 15:13:21 -0500

    Update the "What Does This Code Do?" activity

--- src/prompts/code-reading-prompts.jsx
+++ src/prompts/code-reading-prompts.jsx
@@ -1,33 +1,104 @@
 import { getRandomElement } from "@/utils/arrayUtils";
+import { awsRootDir } from "@/utils/pathUtils";
 
 const divStyle = {
   width: "calc(500px)",
   justifySelf: "center",
+  marginTop: "1rem",
+  marginBottom: "1rem",
 };
 
 const globalImgStyle = {
   width: "100%",
   borderRadius: "8px",
-  marginTop: "2rem",
-  marginBottom: "1rem",
 };
 
 const beginnerCodeBlocks = [
-  { imgName: "move-to-center", style: { width: "50%" } },
-  { imgName: "spin-around-mouse" },
+  {
+    imgName: "move-to-center",
+    style: { width: "50%" },
+    description: (
+      <>
+        <h2>Translate!</h2>
+        <p>Describe what the following code will do:</p>
+      </>
+    ),
+  },
+  {
+    imgName: "spin-around-mouse",
+    style: { width: "90%" },
+    description: (
+      <>
+        <h2>Translate!</h2>
+        <p>Describe what the following code will do:</p>
+      </>
+    ),
+  },
+  {
+    imgName: "shake-around-mouse",
+    style: { width: "90%" },
+    description: (
+      <>
+        <h2>Debug!</h2>
+        <p>
+          The following code is supposed to make a sprite shake (near where it
+          started) when clicked.
+        </p>
+        <p>However, the code is currently not working.</p>
+        <p>Find all the errors!</p>
+      </>
+    ),
+  },
 ];
+
 const experiencedCodeBlocks = [
-  { imgName: "cat-chase-mouse" },
-  { imgName: "max-jump-height", style: { width: "50%" } },
+  {
+    imgName: "cat-chase-mouse",
+    description: (
+      <>
+        <h2>Translate!</h2>
+        <p>Describe what the following code will do:</p>
+      </>
+    ),
+  },
+  {
+    imgName: "max-jump-height",
+    style: { width: "50%" },
+    description: (
+      <>
+        <h2>Translate!</h2>
+        <p>Describe what the following code will do:</p>
+      </>
+    ),
+  },
+  {
+    imgName: "max-number",
+    style: { width: "90%" },
+    description: (
+      <>
+        <h2>Debug!</h2>
+        <p>
+          The following code is supposed to find the largest of a list of
+          numbers.
+        </p>
+        <p>However, the code is currently not working.</p>
+        <p>Find all the errors!</p>
+      </>
+    ),
+  },
 ];
+
+const codeReadingBeginnerPath = `${awsRootDir}/code-reading/beginner`;
+const codeReadingExperiencedPath = `${awsRootDir}/code-reading/experienced`;
 
 export function generateCodeReadingBeginnerPrompt() {
   const promptData = getRandomElement(beginnerCodeBlocks);
-  const imgPath = `https://wizroboticsintro.s3.us-east-2.amazonaws.com/Curriculum/WizSTEMInstitute/TeacherTraining/Scratch/Unit4/ScratchJackbox/code-reading/beginner/${promptData.imgName}.png`;
+  const imgPath = `${codeReadingBeginnerPath}/${promptData.imgName}.png`;
   const imgStyle = { ...globalImgStyle, ...promptData.style };
 
   return (
     <div style={divStyle}>
+      {promptData.description}
       <img src={imgPath} style={imgStyle} />
     </div>
   );
@@ -35,11 +106,12 @@ export function generateCodeReadingBeginnerPrompt() {
 
 export function generateCodeReadingExperiencedPrompt() {
   const promptData = getRandomElement(experiencedCodeBlocks);
-  const imgPath = `https://wizroboticsintro.s3.us-east-2.amazonaws.com/Curriculum/WizSTEMInstitute/TeacherTraining/Scratch/Unit4/ScratchJackbox/code-reading/experienced/${promptData.imgName}.png`;
+  const imgPath = `${codeReadingExperiencedPath}/${promptData.imgName}.png`;
   const imgStyle = { ...globalImgStyle, ...promptData.style };
 
   return (
     <div style={divStyle}>
+      {promptData.description}
       <img src={imgPath} style={imgStyle} />
     </div>
   );
